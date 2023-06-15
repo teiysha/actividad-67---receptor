b8b69cdfb9fb06078bcdc48b9ed30e41c77c8cf9
teiysha <132546302+teiysha@users.noreply.github.com>
2023-06-15 08:57:13 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,23 @@
-
+radio.onReceivedNumber(function (receivedNumber) {
+    let receivednumber = 0
+    led.setBrightness(led.brightness() + receivednumber)
+})
+radio.onReceivedString(function (receivedString) {
+    let ReceivedString = ""
+    if (ReceivedString == "POWER") {
+        encendido = !(encendido)
+        led.setBrightness(125)
+        led.enable(encendido)
+    }
+})
+let encendido = false
+radio.setGroup(1)
+encendido = false
+led.enable(encendido)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
